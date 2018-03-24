1b113ff5dcc93a533fb290414ffd0a504d588290
bw5122 <37465085+bw5122@users.noreply.github.com>
2018-03-25 06:02:45 +0800

    Update search.py

--- gSearch/search.py
+++ gSearch/search.py
@@ -1,8 +1,14 @@
 from googlesearch import search
+from chtext import htmlText
+
 url_list = []
-for url in search('Hong Kong', stop=10):
-            url_list.append(url)
-print(url_list)
+for url in search('Hong Kong', stop=1):
+    url_list.append(url)
+
+for item in url_list:
+    htmlText(item)
+    print('\n'*100)
+
 
 
 
